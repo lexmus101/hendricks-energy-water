26edfcfb9dcce48c46d3e9edb029c7d482396df3
Brian Davidson <brian123513@gmail.com>
2025-11-20 08:01:31 -0800

    Initial Commit

---
+++ src/app/(site)/beforeyoucall/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div>before you call Page</div>
+    )
+}
---
+++ src/app/(site)/services/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+    return (
+        <div>Service Page</div>
+    )
+}
